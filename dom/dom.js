42a1a4bfa04c0a8cb0e8f0f6abf719e7f73cf263
Sebastian Markbage <sema@fb.com>
2020-03-06 09:43:12 -0800

    Demo set up of overrides using nested node_modules

---
+++ dom/dom.js
@@ -0,0 +1,8 @@
+/**
+ * @flow
+ */
+
+import {bar} from 'thing/thing.js';
+
+let x = bar();
+console.log(x.toFixed(2));
---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @flow
+ */
+
+import {bar} from 'thing/thing.js';
+
+let x = bar();
+
+console.log(x);
